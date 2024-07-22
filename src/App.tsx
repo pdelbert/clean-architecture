239266eea10fd42307c529a7c4dff3700fc3e67d
pdelbert <pmarcadel@gmail.com>
2024-07-22 13:06:46 -0300

    Adding - Base Src Structure

---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './scss/index.scss'
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+  </React.StrictMode>,
+)
